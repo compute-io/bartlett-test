1aedf73e682f5d02af5bd741ff9e8576951d2f8b
Planeshifter <pburckhardt@outlook.com>
2015-06-28 14:17:03 +0200

    [UPDATE] keywords, examples, deps

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,13 @@
 'use strict';
 
-var foo = require( './../lib' );
+var bartlett = require( './../lib' ),
+	a,
+	b,
+	out;
+
+a = [ 1, 2, 3 ];
+b = [ 1, 4, 2 ];
+
+out = bartlett( a, b );
+
+console.dir( out );
